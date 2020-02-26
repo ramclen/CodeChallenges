4ef589c04f5aebe8418843a0649df5e1110f0aa3
Cristopher <cristopher.correo@gmail.com>
2020-02-26 20:05:34 +0000

    Added Receiving Message Challenge

---
+++ receiving-messages/readme.md
@@ -0,0 +1,5 @@
+# ReceivingIsThisApp
+
+You're working on a chat app for `WhatlsThisApp`. Someone from your team has committed this code, but there's a problem with it: the `Receiver` doesn't seem to be receiving any messages.
+
+The manager on your team has asked if you can look into this problem to resolve the issue. Can you get the code into a working state?
---
+++ receiving-messages/receiving-messages.js
@@ -0,0 +1,35 @@
+function receivingMessages (messages) {
+  class Emitter {
+    constructor(messages = []) {
+      this.messages = messages;
+      this.event = () => {};
+    }
+
+    setEvent(fn) {
+      this.event = fn;
+    }
+
+    trigger() {
+      this.messages.forEach(message => this.event(message));
+    }
+  }
+
+  class Receiver {
+    constructor() {
+      this.messages = [];
+    }
+
+    ping(message) {
+      this.messages.push(message);
+    }
+  }
+
+  const myEmitter = new Emitter(messages);
+  const myReceiver = new Receiver();
+
+  myEmitter.setEvent(myReceiver.ping);
+  myEmitter.trigger();
+
+  return myReceiver.messages;
+};
+
